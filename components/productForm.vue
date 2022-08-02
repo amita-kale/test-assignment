77110d0568f3ee2d302423e7a85119ad3980c4c1
amita-kale <amitakale297@gmail.com>
2022-08-02 15:40:20 +0530

    filter value on tab

--- components/productForm.vue
+++ components/productForm.vue
@@ -21,7 +21,7 @@
                     <select v-model="product.category" @change="onChangeOfCategory($event)">
                         <option value='Cloths'>Cloths</option>
                          <option value="Shoes">Shoes</option>
-                          <option value="Guiter">Guiter</option>
+                          <option value="Guitar">Guitar</option>
                     </select>
                 </tr>
                 <tr>
@@ -56,9 +56,9 @@
         class="sm:relative w-full sm:flex  sm:  items-center sm:justify-between py-2  bg-purple-300 text-black-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
         <div class="sm:container-fluid w-full sm:flex  items-center justify-between px-2">
         <div class="sm:container-fluid">
-        <a class="sm:text-xl sm:p-2 m-3 text-white hover:bg-blue-300" @click="searchInput($event)" href="#">Cloths</a>
-        <a class="sm:text-xl sm:p-2 m-4 text-white hover:bg-blue-300" href="#">Shoes</a>
-        <a class="sm:text-xl sm:p-2 m-4 text-white hover:bg-red-300" href="#">Guitar</a>  
+        <a class="sm:text-xl sm:p-2 m-3 text-white hover:bg-blue-300" @click="searchOnlyCloth">Cloths</a>
+        <a class="sm:text-xl sm:p-2 m-4 text-white hover:bg-blue-300" @click="searchOnlyShoes" >Shoes</a>
+        <a class="sm:text-xl sm:p-2 m-4 text-white hover:bg-red-300" @click="searchOnlyGuitar">Guitar</a>  
         <input class="placeholder:italic  bg-white w-50 border border-slate-300 rounded-md m-5 py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for Category..." type="text" @input="searchInput($event)"  name="search"/>
 
       </div>
@@ -77,9 +77,46 @@
            Category:<b>{{prod.category}}</b>
         </div>
         
+    </div>
 
+     <div v-if="this.shoes==true" class="flex">
+       
+        <div class="m-10 h-52 w-52 border-4 border-indigo-600" v-for="item in this.shoesArr" :key="item">
+        <!-- <span v-if="item.category =='Shoes'"> -->
+            <img :src="item.img" class="h-32 w-52"> 
+            product Name: <b>{{item.productname}}</b>
+            prize:  <b>{{item.prize}}</b>
+            Category:<b>{{item.category}}</b>
+        <!-- </span> -->
+        </div>
         
     </div>
+    <div v-else-if="this.cloth==true" class="flex">
+       
+        <div class="m-10 h-52 w-52 border-4 border-indigo-600" v-for="item in this.shoesArr" :key="item">
+        <!-- <span v-if="item.category =='Shoes'"> -->
+            <img :src="item.img" class="h-32 w-52"> 
+            product Name: <b>{{item.productname}}</b>
+            prize:  <b>{{item.prize}}</b>
+            Category:<b>{{item.category}}</b>
+        <!-- </span> -->
+        </div>
+        
+    </div>
+
+     <div v-else-if="this.guitar==true" class="flex">
+       
+        <div class="m-10 h-52 w-52 border-4 border-indigo-600" v-for="item in this.shoesArr" :key="item">
+        <!-- <span v-if="item.category =='Shoes'"> -->
+            <img :src="item.img" class="h-32 w-52"> 
+            product Name: <b>{{item.productname}}</b>
+            prize:  <b>{{item.prize}}</b>
+            Category:<b>{{item.category}}</b>
+        <!-- </span> -->
+        </div>
+        
+    </div>
+
     </div>
 </div>
 
@@ -90,6 +127,10 @@ export default {
    data() {
     return {
     products: [],
+    cloth:false,
+    shoes:false,
+    guitar:false,
+    shoesArr : [],    
       product: {
         productname: null,
         prize: null,
@@ -110,8 +151,7 @@ export default {
   methods: {
     createNewCard(){
          this.products.push(this.product);
-        //   sessionStorage.setItem('productdata', JSON.stringify(this.product)); 
-        // const products = JSON.parse(sessionStorage.getItem('productdata')); 
+       
            this.product= {
             productname: '',
             prize: '',
@@ -130,9 +170,36 @@ export default {
         this.product.img='Assetss/guiter.jpg';
       }
     },
+     searchOnlyCloth(){
+      
+        this.cloth=true;
+        this.shoesArr = this.products.filter(e => {
+            if(e.category=='Cloths'){
+                return e;
+            }
+        })
+    },
+    searchOnlyGuitar(){
+      
+        this.guitar=true;
+        this.shoesArr = this.products.filter(e => {
+            if(e.category=='Guitar'){
+                return e;
+            }
+        })
+    },
+    searchOnlyShoes(){
+      
+        this.shoes=true;
+        this.shoesArr = this.products.filter(e => {
+            if(e.category=='Shoes'){
+                return e;
+            }
+        })
+    },
      searchInput(evt){
-      // console.log(evt.target.value);
       this.searchText = evt.target.value;
+       
     },
 
 
